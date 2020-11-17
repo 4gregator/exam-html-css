ccc65e1d20fd243c7e84dfb24c345afdb4b2c566
Agregat <ikrasnobai@yandex.ru>
2020-11-17 18:24:20 +0300

    8-9

--- 8/main.js
+++ 8/main.js
@@ -1,20 +1,21 @@
-document.addEventListener( 'DOMContentLoaded', smooth.bind(this, 'smooth-scroll__item', 0) );
-window.addEventListener( 'scroll', smooth.bind(this, 'smooth-scroll__item', 300) );
+$(function () {
+  $('.expand__title').on('click', function () {
+    $('.expand__text_visible').slideUp().removeClass('expand__text_visible');
+    $(this).parent().find('.expand__text').slideDown().addClass('expand__text_visible');
 
-
-function smooth(clas, timeout) {
-  const scroll = window.pageYOffset + window.innerHeight;
-  const elems = document.getElementsByClassName(clas);
-  const visible = clas + '_visible';
-
-  Array.prototype.forEach.call(elems, function (el) {
-    let offset = el.offsetTop;
-
-    if (!el.classList.contains(visible) && scroll > offset) {
-      setTimeout(function() {
-        el.classList.add(visible);
-      }, timeout);
-    }
+    // второй вариант через вызов функции
+    //expand.call(this, 'expand__text');
   });
-  
+});
+
+
+// target (string) = имя класса скрываемого/показываемого контента
+// duration (integer) = скорость анимации слайда
+function expand(target, duration = 400) {
+  const targetClass = '.' + target;
+  const visible = target + '_visible';
+  const visibleClass = '.' + visible;
+
+  $(visibleClass).slideUp(duration).removeClass(visible);
+  $(this).parent().find(targetClass).slideDown(duration).addClass(visible);
 }
