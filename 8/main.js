2eb405b7b1bc478c34979fcd8d4debee80f3c10c
Agregat <ikrasnobai@yandex.ru>
2020-11-18 09:04:49 +0300

    fix

--- 8/main.js
+++ 8/main.js
@@ -1,16 +1,13 @@
 $(function () {
   $('.expand__title').on('click', function () {
-    $('.expand__text_visible').slideUp().removeClass('expand__text_visible');
-    $(this).parent().find('.expand__text').slideDown().addClass('expand__text_visible');
-
-    // второй вариант через вызов функции
-    //expand.call(this, 'expand__text');
+    expand.call(this, 'expand__text');
   });
 });
 
 
 // target (string) = имя класса скрываемого/показываемого контента
 // duration (integer) = скорость анимации слайда
+// в this должен передаваться элемент, по которому произведён клик
 function expand(target, duration = 400) {
   const targetClass = '.' + target;
   const visible = target + '_visible';
